004221aca21f89aa3c45c390064a8e960f29af34
SymphonyOfFear <mikeykouw@hotmail.com>
2024-10-22 11:07:25 +0200

    A

---
+++ resources/views/layouts/navigation/student.blade.php
@@ -0,0 +1,3 @@
+<x-nav-link :href="route('student.results')" :active="request()->routeIs('student.results')">
+    {{ __('Resultaten overzicht') }}
+</x-nav-link>
---
+++ resources/views/layouts/navigation/teacher.blade.php
@@ -0,0 +1,3 @@
+<x-nav-link :href="route('teacher.results')" :active="request()->routeIs('teacher.results')">
+    {{ __('Docenten Resultaten overzicht') }}
+</x-nav-link>
